8e80cb7baf5e71abf297e36dcfcc77be9c634d80
wblak <blakew@uwm.edu>
2024-04-16 22:51:57 -0500

    added user base class and admin subclass with stub methods

---
+++ classes/AdminClass.py
@@ -0,0 +1,44 @@
+from classes.UserClass import UserClass
+
+
+class Admin(UserClass):
+
+    def create_user(self, username, password, name, role, email, phone_number, address):
+        pass
+
+    def delete_user(self, user_id):
+        pass
+
+    def create_course(self, course_id, course_name, description):
+        pass
+
+    def create_section(self, course_id, section_id, meeting_time, section_type):
+        pass
+
+    def assign_instructor(self, user_id, section_id):
+        pass
+
+    def assign_ta(self, user_id, section_id):
+        pass
+
+    def edit_user(self, username, password, name, role, email, phone, address):
+        pass
+
+    def edit_course(self, course_id, course_name, description):
+        pass
+
+    def delete_course(self, course_id):
+        pass
+
+    def edit_section(self, course_id, section_id, meeting_time, section_type):
+        pass
+
+    def delete_section(self, section_id):
+        pass
+
+    def message_all(self, message):
+        pass
+
+    # Override
+    def get_contact_info(self, user_id):
+        pass
