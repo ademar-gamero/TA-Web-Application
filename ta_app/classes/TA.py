93e6bdfd1ac48e11d1ca7fcf70f7ca0cfc510519
ademar <aegamero@uwm.edu>
2024-04-17 17:08:44 -0500

    add new email_validate function, and tests

--- ta_app/classes/TA.py
+++ ta_app/classes/TA.py
@@ -1,7 +1,13 @@
 from . import ClassUser
+from django.core.validators import EmailValidator
 
-class TeacherAssistant(ClassUser):
+class TA(ClassUser):
+
     def edit_contact_info(self,new_contact_info):
-        pass
+        pass       
+
     def view_contact_info(self):
+        pass
+
+    def valid_email(self,email):
         pass
