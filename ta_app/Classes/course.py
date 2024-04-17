26b55b8a647510b99088ab134ea1606dd6242e75
LNelson100 <nelso862@uwm.edu>
2024-04-17 06:46:32 -0500

    Added Courses skeleton.

---
+++ ta_app/Classes/course.py
@@ -0,0 +1,39 @@
+class Course(object):
+    def __init__(self, course_id=0, course_name="unavailable", description="TBD"):
+        self.course_id = course_id
+        self.course_name = course_name
+        self.description = description
+        pass
+
+    def __str__(self):
+        pass
+
+    def add_name(self, course_name):
+        pass
+
+    def add_id(self, course_id):
+        pass
+
+    def add_description(self, description):
+        pass
+
+    def set_name(self, course_name):
+        pass
+
+    def set_id(self, course_id):
+        pass
+
+    def set_description(self, description):
+        pass
+
+    def delete_course(self, course_name):
+        pass
+
+    def get_course_id(self):
+        pass
+
+    def get_course_name(self):
+        pass
+
+    def get_course_description(self):
+        pass
