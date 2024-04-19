61f30e4b0178ce5e6fdb48b9ee55da96f0b044d1
LNelson100 <nelso862@uwm.edu>
2024-04-18 23:22:27 -0500

    Removed context assertEqual statements from init tests & altered expected str representation.

--- ta_app/tests.py
+++ ta_app/tests.py
@@ -25,53 +25,46 @@ class CourseInit(CommonCourses):
 
     def test_idInputString(self):
         # will constructor throw an exception if string data is passed to the course_id?
-        with self.assertRaises(TypeError, msg="Non-numeric ID data fails to raise TypeError") as ctx:
+        with self.assertRaises(TypeError, msg="Non-numeric ID data fails to raise TypeError"):
             Course("X", "test name", "test description")
-        self.assertEqual(str(ctx.exception), "Non-numeric ID data fails to raise TypeError")
 
     def test_idInputFloat(self):
         # will constructor throw an exception if floating-point data is passed to the course_id?
-        with self.assertRaises(TypeError, msg="Float ID data fails to raise TypeError") as ctx:
+        with self.assertRaises(TypeError, msg="Float ID data fails to raise TypeError"):
             Course(1.1, "test name", "test description")
-        self.assertEqual(str(ctx.exception), "Float ID data fails to raise TypeError")
 
     def test_idInputNegative(self):
         # will constructor throw an exception if negative data is passed to the course_id?
         # Rule: negative IDs will not be allowed
-        with self.assertRaises(ValueError, msg="Negative ID data fails to raise ValueError") as ctx:
+        with self.assertRaises(ValueError, msg="Negative ID data fails to raise ValueError"):
             Course(-1, "test name", "test description")
-        self.assertEqual(str(ctx.exception), "Negative ID data fails to raise ValueError")
 
     def test_nameInputInt(self):
         # will constructor throw an exception if int data is passed to the course_name?
-        with self.assertRaises(TypeError, msg="Integer name data fails to raise TypeError") as ctx:
+        with self.assertRaises(TypeError, msg="Integer name data fails to raise TypeError"):
             Course(0, 1, "test description")
-        self.assertEqual(str(ctx.exception), "Integer name data fails to raise TypeError")
 
     def test_nameInputFloat(self):
         # will constructor throw an exception if floating-point data is passed to the course_name?
-        with self.assertRaises(TypeError, msg="Float name data fails to raise TypeError") as ctx:
+        with self.assertRaises(TypeError, msg="Float name data fails to raise TypeError"):
             Course(0, 1.1, "test description")
-        self.assertEqual(str(ctx.exception), "Float name data fails to raise TypeError")
 
     def test_descriptionInputInt(self):
         # will constructor throw an exception if int data is passed to the description?
-        with self.assertRaises(TypeError, msg="Integer description data fails to raise TypeError") as ctx:
+        with self.assertRaises(TypeError, msg="Integer description data fails to raise TypeError"):
             Course(0, "test name", 1)
-        self.assertEqual(str(ctx.exception), "Integer description data fails to raise TypeError")
 
     def test_descriptionInputFloat(self):
         # will constructor throw an exception if floating-point data is passed to the description?
-        with self.assertRaises(TypeError, msg="Float description data fails to raise TypeError") as ctx:
+        with self.assertRaises(TypeError, msg="Float description data fails to raise TypeError"):
             Course(0, "test name", 1.1)
-        self.assertEqual(str(ctx.exception), "Float description data fails to raise TypeError")
 
 
 class CourseString(CommonCourses):
     def test_displayCourse(self):
         # will string method produce the correct output
-        # Rule: string representation will be "<course_name>"
-        self.assertEqual("test name", str(self.default), "incorrect string representation returned")
+        # Rule: string representation will be "<course_name> - <course_id>"
+        self.assertEqual("test name - 0", str(self.default), "incorrect string representation returned")
 
 
 class CourseSetID(CommonCourses):
