cbde01d6b4e827231ff85831f966767cc3c7e3ec
wblak <blakew@uwm.edu>
2024-04-20 16:49:44 -0500

    working on AdminTests

--- classes/AdminClass.py
+++ classes/AdminClass.py
@@ -3,34 +3,34 @@ from classes.UserClass import UserClass
 
 class Admin(UserClass):
 
-    def create_user(self, username, password, name, role, email, phone_number, address):
+    def create_user(self, username, password, name, role, email, phone_number=None, address=None):
         pass
 
     def delete_user(self, user_id):
         pass
 
-    def create_course(self, course_id, course_name, description):
+    def create_course(self, course_id, course_name, description=None):
         pass
 
     def create_section(self, course_id, section_id, meeting_time, section_type):
         pass
 
-    def assign_instructor(self, user_id, section_id):
+    def assign_instructor(self, user, section_id):
         pass
 
-    def unassign_instructor(self, user_id, section_id):
+    def unassign_instructor(self, user, section_id):
         pass
 
-    def assign_ta(self, user_id, section_id):
+    def assign_ta(self, user, section_id):
         pass
 
-    def unassign_ta(self, user_id, section_id):
+    def unassign_ta(self, user, section_id):
         pass
 
-    def edit_user(self, username, password, name, role, email, phone, address):
+    def edit_user(self, user, username=None, password=None, name=None, role=None, email=None, phone=None, address=None):
         pass
 
-    def edit_course(self, course_id, course_name, description):
+    def edit_course(self, course_id, course_name=None, description=None):
         pass
 
     def delete_course(self, course_id):
--- ta_app/tests/AdminTests.py
+++ ta_app/tests/AdminTests.py
@@ -3,18 +3,98 @@ from classes.AdminClass import Admin
 
 
 class TestAdminClass(TestCase):
+    ad = None
 
-    def test_createUser(self):
-        pass
+    def setUp(self):
+        self.ad = Admin("admin", "admin", "Admin", "AD", "admin@admin.com")
+
+    def test_createUserTA(self):
+        user = self.ad.create_user("newuser", "password", "NewTA", "TA", "email@uwm.edu")
+
+    def test_createUserInstructor(self):
+        user = self.ad.create_user("newuser", "password", "NewIN", "IN", "email@uwm.edu")
+
+    def test_createUserAdmin(self):
+        user = self.ad.create_user("newuser", "password", "NewAD", "AD", "email@uwm.edu")
+
+    def test_createUserFullDetails(self):
+        user = self.ad.create_user("newuser", "password", "New", "IN", "email@uwm.edu",
+                                   "555-5555", "123 Fake Street")
 
     def test_delete_user(self):
-        pass
+        user = self.ad.create_user("newuser", "password", "New", "TA", "email@uwm.edu")
+        self.ad.delete_user(user)
 
-    def test_edit_user(self):
-        pass
+    def test_delete_userNull(self):
+        with self.assertRaises(ValueError, msg="Fails to catch wrong input type"):
+            self.ad.delete_user(None)
+
+    def test_delete_userNonUser(self):
+        with self.assertRaises(ValueError, msg="Fails to catch wrong input type"):
+            self.ad.delete_user("Tacos")
+
+    def test_edit_userFull(self):
+        user = self.ad.create_user("newuser", "password", "New", "TA", "email@uwm.edu")
+        self.ad.edit_user(user, "newUsername", "newPass", "New Name", "IN", "newemail@uwm.edu",
+                          "555-5555", "123 Fake Street")
+
+    def test_edit_userUsername(self):
+        user = self.ad.create_user("newuser", "password", "New", "TA", "email@uwm.edu")
+        self.ad.edit_user(user, "newUsername")
+
+    def test_edit_userUsernameEmpty(self):
+        user = self.ad.create_user("newuser", "password", "New", "TA", "email@uwm.edu")
+        with self.assertRaises(ValueError, msg="Fails to catch empty username"):
+            self.ad.edit_user(user, "")
+
+    def test_edit_userPassword(self):
+        user = self.ad.create_user("newuser", "password", "New", "TA", "email@uwm.edu")
+        self.ad.edit_user(user, password="newPass")
+
+    def test_edit_userPasswordEmpty(self):
+        user = self.ad.create_user("newuser", "password", "New", "TA", "email@uwm.edu")
+        with self.assertRaises(ValueError, msg="Fails to catch empty password"):
+            self.ad.edit_user(user, password="")
+
+    def test_edit_userName(self):
+        user = self.ad.create_user("newuser", "password", "New", "TA", "email@uwm.edu")
+        self.ad.edit_user(user, name="New Name")
+
+    def test_edit_userNameEmpty(self):
+        user = self.ad.create_user("newuser", "password", "New", "TA", "email@uwm.edu")
+        with self.assertRaises(ValueError, msg="Fails to catch empty name"):
+            self.ad.edit_user(user, name="")
+
+    def test_edit_userNameNonString(self):
+        user = self.ad.create_user("newuser", "password", "New", "TA", "email@uwm.edu")
+        with self.assertRaises(ValueError, msg="Fails to catch invalid value type"):
+            self.ad.edit_user(user, name=123)
 
     def test_create_course(self):
-        pass
+        course = self.ad.create_course(101, "English", "A generic class")
+
+    def test_create_courseNoDescription(self):
+        course = self.ad.create_course(101, "English")
+
+    def test_create_courseNull(self):
+        with self.assertRaises(ValueError, msg="Fails to catch null input"):
+            course = self.ad.create_course(None, None, None)
+
+    def test_create_courseNullID(self):
+        with self.assertRaises(ValueError, msg="Fails to catch null input"):
+            course = self.ad.create_course(None, "Math", None)
+
+    def test_create_courseEmptyID(self):
+        with self.assertRaises(ValueError, msg="Fails to catch empty input"):
+            course = self.ad.create_course("", "Math", None)
+
+    def test_create_courseNullName(self):
+        with self.assertRaises(ValueError, msg="Fails to catch null input"):
+            course = self.ad.create_course(101, None, None)
+
+    def test_create_courseEmptyName(self):
+        with self.assertRaises(ValueError, msg="Fails to catch empty input"):
+            course = self.ad.create_course(101, "", None)
 
     def test_edit_course(self):
         pass
