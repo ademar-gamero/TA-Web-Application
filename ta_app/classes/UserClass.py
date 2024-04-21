a383252b5d871d9096fd23dbdfa4e370602bc02f
ademar <aegamero@uwm.edu>
2024-04-21 18:18:40 -0500

    remove getters, create two new methods for UserClass,create integration tests

--- ta_app/classes/UserClass.py
+++ ta_app/classes/UserClass.py
@@ -1,10 +1,13 @@
 from abc import ABC
 from ta_app.models import User, Roles, Section
 
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+
 
 class UserClass(ABC):
 
-    def __init__(self, username, password, name, role, email, phone_number="", address="",assigned = False,assigned_sections=[]):
+    def __init__(self, username, password, name, role, email, phone_number="", address="",assigned = False, assigned_sections=[]):
         self.username = username
         self.password = password
         self.name = name
@@ -48,35 +51,14 @@ class UserClass(ABC):
     def remove_section(self, section_to_remove):
         pass
 
-    def get_username(self):
-        return self.username
-
-    def get_password(self):
-        return self.password
-
-    def get_name(self):
-        return self.name
-
-    def get_role(self):
-        return self.role
-
-    def get_email(self):
-        return self.email
-
-    def get_phone_number(self):
-        return self.phone_number
-
-    def get_address(self):
-        return self.address
-
-    def get_assigned(self):
-        return self.assigned
-
-    def get_assigned_sections(self):
-        return self.assigned_sections
-
     def set_contact_info(self, email, phone_number, address):
         pass
 
     def get_contact_info(self, user_id):
         pass
+
+    def create_user(self):
+        pass
+    def delete_user(self):
+        pass
+
--- ta_app/tests/UserTests.py
+++ ta_app/tests/UserTests.py
@@ -1,8 +1,16 @@
 from django.test import TestCase
 from ta_app.classes.UserClass import UserClass
-
+from ta_app.models import Course, Section, User
+from datetime import datetime
 
 class TestUserClass(TestCase):
+
+    def setUp(self):
+        date_string = "Tue 2:30pm"
+        date_object = datetime.strptime(date_string, "%a %H:%M%p")
+
+        self.admin = User(name="ad",username="admin",password="admin",email="admin@email.com",role="Admin",phone_number=1,address="1",assigned=False)
+        self.admin.save()
 
     def test_createNullAll(self):
         with self.assertRaises(ValueError, msg="Fails to catch invalid entry"):
@@ -94,12 +102,12 @@ class TestUserClass(TestCase):
             temp = UserClass("username", "pass", "name", "Batman", "email@email.com")
 
     def test_createAdmin(self):
-        temp = UserClass(username="new", password="password", name="New", email="new@uwm.edu", role="AD")
+        temp = UserClass(username="new", password="password", name="New", email="new@uwm.edu", role="Admin")
         self.assertEqual(temp.username, "new", "Username is wrong")
         self.assertEqual(temp.password, "password", "Password is wrong")
         self.assertEqual(temp.name, "New", "Name is wrong")
         self.assertEqual(temp.email, "new@uwm.edu", "Email is wrong")
-        self.assertEqual(temp.role, "AD", "Role is wrong")
+        self.assertEqual(temp.role, "Admin", "Role is wrong")
         self.assertEqual(temp.phone_number, "", "Phone number should be empty")
         self.assertEqual(temp.address, "", "Address should be empty")
         self.assertEqual(temp.assigned, False, "Should not be assigned")
@@ -108,12 +116,12 @@ class TestUserClass(TestCase):
     # check if creating an Admin with all necessary inputs returns successfully
 
     def test_createInstructor(self):
-        temp = UserClass(username="new", password="password", name="New", email="new@uwm.edu", role="IN")
+        temp = UserClass(username="new", password="password", name="New", email="new@uwm.edu", role="Instructor")
         self.assertEqual(temp.username, "new", "Username is wrong")
         self.assertEqual(temp.password, "password", "Password is wrong")
         self.assertEqual(temp.name, "New", "Name is wrong")
         self.assertEqual(temp.email, "new@uwm.edu", "Email is wrong")
-        self.assertEqual(temp.role, "IN", "Role is wrong")
+        self.assertEqual(temp.role, "Instructor", "Role is wrong")
         self.assertEqual(temp.phone_number, "", "Phone number should be empty")
         self.assertEqual(temp.address, "", "Address should be empty")
         self.assertEqual(temp.assigned, False, "Should not be assigned")
@@ -122,12 +130,12 @@ class TestUserClass(TestCase):
     # check if creating an Instructor with all necessary inputs returns successfully
 
     def test_createTA(self):
-        temp = UserClass(username="new", password="password", name="New", email="new@uwm.edu", role="TA")
+        temp = UserClass(username="new", password="password", name="New", email="new@uwm.edu", role="Teacher-Assistant")
         self.assertEqual(temp.username, "new", "Username is wrong")
         self.assertEqual(temp.password, "password", "Password is wrong")
         self.assertEqual(temp.name, "New", "Name is wrong")
         self.assertEqual(temp.email, "new@uwm.edu", "Email is wrong")
-        self.assertEqual(temp.role, "TA", "Role is wrong")
+        self.assertEqual(temp.role, "Teacher-Assistant", "Role is wrong")
         self.assertEqual(temp.phone_number, "", "Phone number should be empty")
         self.assertEqual(temp.address, "", "Address should be empty")
         self.assertEqual(temp.assigned, False, "Should not be assigned")
@@ -149,4 +157,36 @@ class TestUserClass(TestCase):
         self.assertEqual(temp.assigned_sections, None, "Shouldn't be assigned to any sections")
         self.assertEqual(temp.__str__(), "New : AD", "toString returns incorrectly")
     # check if creating a user with additional details (phone/address) returns successfully
+
+    def test_createToDB(self):
+        date_string = "Tue 2:30pm"
+        date_object = datetime.strptime(date_string, "%a %H:%M%p")
+        algos = Course(course_id=351,course_name="compsci",description="blah blah blah")
+        algos.save() 
+
+        section = Section(course_parent=algos,section_id=12345,meeting_time = date_object,type="Lecture")
+        section.save()
+
+        slist = [section]
+        self.assigned_user = UserClass("ta","ta","apoorv","Teacher-Assistant","email@email.com","1","1",True,slist)
+        self.assigned_user.create_user()
+        db_user = User.objects.get(name="apoorv")
+
+        self.assertEqual(db_user.username, self.assigned_user.username, "Username is wrong")
+        self.assertEqual(db_user.password,self.assigned_user.password, "Password is wrong")
+        self.assertEqual(db_user.name, self.assigned_user.name, "Name is wrong")
+        self.assertEqual(db_user.role, self.assigned_user.role, "Role is wrong")
+        self.assertEqual(db_user.email, self.assigned_user.email, "Email is wrong")
+        self.assertEqual(db_user.phone_number, self.assigned_user.phone_number, "Phone is wrong")
+        self.assertEqual(db_user.address, self.assigned_user.address, "Address is wrong")
+        self.assertEqual(db_user.assigned, self.assigned_user.assigned, "Assigned is incorrect")
+        j = 0;
+        for i in db_user.assigned_section.all():
+            self.assertEqual(self.assigned_user.assigned_sections[j],i,"assigned sections not present")
+            j=j+1
+
+    def test_deleteToDB(self):
+        self.assigned_user.delete_user()
+        db_user = User.objects.filter(name="apoorv")
+        self.assertEqual(db_user,None,"accoutn was not deleted from the db")
 
