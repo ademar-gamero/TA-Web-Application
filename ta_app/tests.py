85f0f72cdefad8266db04a7de6073cf663e5925d
LNelson100 <nelso862@uwm.edu>
2024-04-21 23:25:21 -0500

    New unit tests for create_course method. Removed default values init tests.

--- ta_app/tests.py
+++ ta_app/tests.py
@@ -1,11 +1,13 @@
 from django.test import TestCase
-from ta_app.Classes.Course import Course
+from ta_app.Classes.CourseClass import CourseClass
 
 
 # Create your tests here.
 class CommonCourses(TestCase):
     def setUp(self):
-        self.default = Course(0, "test name", "test description")
+        self.default = CourseClass(0, "test name", "test description")
+        self.cs361 = CourseClass(361, "CompSci", "A computer science course")
+        self.duplicateDefault = CourseClass(0, "test name", "test description")
 
 
 class CourseInit(CommonCourses):
@@ -16,48 +18,41 @@ class CourseInit(CommonCourses):
         self.assertEqual("test name", temp.course_name, "course_name is incorrect value")
         self.assertEqual("test description", temp.description, "course description is incorrect value")
 
-    def test_defaultCourse(self):
-        # will the 0 argument version of the constructor produce the correct id, name, and description?
-        temp = Course()
-        self.assertEqual(0, temp.course_id, "Default ID is incorrect value")
-        self.assertEqual("unavailable", temp.course_name, "Default name is incorrect value")
-        self.assertEqual("TBD", temp.description, "Default description is incorrect value")
-
     def test_idInputString(self):
         # will constructor throw an exception if string data is passed to the course_id?
         with self.assertRaises(TypeError, msg="Non-numeric ID data fails to raise TypeError"):
-            Course("X", "test name", "test description")
+            CourseClass("X", "test name", "test description")
 
     def test_idInputFloat(self):
         # will constructor throw an exception if floating-point data is passed to the course_id?
         with self.assertRaises(TypeError, msg="Float ID data fails to raise TypeError"):
-            Course(1.1, "test name", "test description")
+            CourseClass(1.1, "test name", "test description")
 
     def test_idInputNegative(self):
         # will constructor throw an exception if negative data is passed to the course_id?
         # Rule: negative IDs will not be allowed
         with self.assertRaises(ValueError, msg="Negative ID data fails to raise ValueError"):
-            Course(-1, "test name", "test description")
+            CourseClass(-1, "test name", "test description")
 
     def test_nameInputInt(self):
         # will constructor throw an exception if int data is passed to the course_name?
         with self.assertRaises(TypeError, msg="Integer name data fails to raise TypeError"):
-            Course(0, 1, "test description")
+            CourseClass(0, 1, "test description")
 
     def test_nameInputFloat(self):
         # will constructor throw an exception if floating-point data is passed to the course_name?
         with self.assertRaises(TypeError, msg="Float name data fails to raise TypeError"):
-            Course(0, 1.1, "test description")
+            CourseClass(0, 1.1, "test description")
 
     def test_descriptionInputInt(self):
         # will constructor throw an exception if int data is passed to the description?
         with self.assertRaises(TypeError, msg="Integer description data fails to raise TypeError"):
-            Course(0, "test name", 1)
+            CourseClass(0, "test name", 1)
 
     def test_descriptionInputFloat(self):
         # will constructor throw an exception if floating-point data is passed to the description?
         with self.assertRaises(TypeError, msg="Float description data fails to raise TypeError"):
-            Course(0, "test name", 1.1)
+            CourseClass(0, "test name", 1.1)
 
 
 class CourseString(CommonCourses):
@@ -148,3 +143,55 @@ class CourseGetCourseDescription(CommonCourses):
         # will get_course_description return the course's current description?
         self.assertEqual("test description", self.default.get_course_description(),
                          "Value returned by get_course_description is incorrect")
+
+
+class CourseCreateCourse(CommonCourses):
+    def test_addSingle(self):
+        # will the create_course method return the correct value when a single course is added?
+        temp = self.default
+        self.assertEqual(True, temp.create_course(), "Adding course did not return True")
+
+    def test_addMultiple(self):
+        # will the create_course method allow adding two courses with all different fields?
+        temp1 = self.default
+        temp2 = self.cs361
+        temp1.create_course()
+        self.assertEqual(True, temp2.create_course(), "Adding second course did not return True")
+
+    def test_addSameTwice(self):
+        # will the create_course method return False when trying to add the same course twice?
+        temp = self.default
+        temp.create_course()
+        self.assertEqual(False, temp.create_course(), "Adding the same course twice did not return False")
+
+    def test_addDuplicate(self):
+        # will the create_course method return False when trying to add a course with all duplicate fields?
+        temp1 = self.default
+        temp2 = self.duplicateDefault
+        temp1.create_course()
+        self.assertEqual(False, temp2.create_course(), "Adding a duplicate course did not return False")
+
+    def test_addDuplicateDifferentDescription(self):
+        # will trying to call create_course with a course with same ID and same name,
+        # but different description be allowed?
+        temp1 = self.default
+        temp2 = CourseClass(0, "test name", "new description")
+        temp1.create_course()
+        self.assertEqual(False, temp2.create_course(),
+                         "Adding a duplicate course with different description did not return False")
+
+    def test_addSameNameDifferentID(self):
+        # will create_course allow two courses with the same name but different IDs to be added?
+        temp1 = self.cs361
+        temp2 = CourseClass(351, "CompSci", "A computer science course")
+        temp1.create_course()
+        self.assertEqual(True, temp2.create_course(),
+                         "Adding a course with same name but different ID did not return True")
+
+    def test_addSameIDDifferentName(self):
+        # will create_course allow two courses with the same ID but different name be added?
+        temp1 = self.cs361
+        temp2 = CourseClass(361, "Anthro", "An anthropology course")
+        temp1.create_course()
+        self.assertEqual(True, temp2.create_course(),
+                         "Adding a course with same ID but different name did not return True")
