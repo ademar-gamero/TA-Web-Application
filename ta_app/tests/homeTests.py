bb12fa7824ef85f479ffe907a43c055c76f6c021
ademar <aegamero@uwm.edu>
2024-04-22 04:24:08 -0500

    Home acceptance test rename, home template link added, add create_course.html if check to signal failure or success of course creation

---
+++ ta_app/tests/homeTests.py
@@ -0,0 +1,49 @@
+from django.urls import reverse
+from django.test import TestCase, Client
+from ta_app.models import User
+class home(TestCase):
+    green=None
+    def setUp(self):
+        self.green = Client()
+        User(name="admin",username="admin",password="admin",email="admin@email.com",role="Admin",phone_number=1,address="1",assigned=False).save()
+        User(name="instructor",username="instructor",password="instructor",email="instructor@email.com",role="Instructor",phone_number=1,address="1",assigned=False).save()       
+        self.Ausername = "admin"
+        self.Apassword = "admin"
+        self.Iusername = "instructor"
+        self.Ipassword = "instructor"
+
+    def test_roleValidationAdminAccountList(self):
+        resp = self.green.post("/login/",{"username":self.Ausername,"password":self.Apassword},follow=True)
+        self.assertEqual(resp.url, '/Home/')
+        resp = self.green.get("/Home/accountList/")
+        self.assertEqual(200,resp.status_code,"role validation failed")
+
+    def test_rvAdminCorrectLinks(self):
+        resp = self.green.post("/login/",{"username":self.Ausername,"password":self.Apassword},follow=True)
+        self.assertEqual(resp.url, '/Home/')
+        resp = self.green.get('/Home/')
+        self.assertContains(resp,'<a href="%s">Course Catalog</a>' % reverse("courseList"),"missing link",html=True)
+        self.assertContains(resp,'<a href="%s">Create Course</a>' % reverse("createAccount"),"missing link",html=True)
+        self.assertContains(resp,'<a href="%s">Account List</a>' % reverse("accountList"),"missing link",html=True)
+        self.assertContains(resp,'<a href="%s">Create Account</a>' % reverse("createAccount"),"missing link",html=True)
+        self.assertContains(resp,'<a href="%s">Create Section</a>' % reverse("createSection"),"missing link",html=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
