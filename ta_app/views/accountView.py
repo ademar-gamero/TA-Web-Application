72b9a82c05c14ed8160ec4ab479a75fd3b6b0768
ademar <aegamero@uwm.edu>
2024-04-23 03:36:27 -0500

    fix account html, modify accountView,

--- ta_app/views/accountView.py
+++ ta_app/views/accountView.py
@@ -7,12 +7,13 @@ from ta_app.classes.UserClass import UserClass
 class accountView(View):
     acc_edit = None
     acc_pk = None
+    val = None
     def get(self,request,pk):
         self.acc_pk = pk
         account = User.objects.get(pk=self.acc_pk) 
         self.acc_edit = UserClass(account.username,account.password,account.name,account.role,
         account.email,account.phone_number,account.address,account.assigned,account.assigned_sections)
-        return render(request,"courseList.html",{'user': account})
+        return render(request,"courseList.html",{'user': account,"check":self.val})
 
     def post(self,request):
         name = request.POST.get('name')    
@@ -22,13 +23,15 @@ class accountView(View):
         phone_number = request.POST.get('phone_number')    
         role = request.POST.get('role')    
         address = request.POST.get('address')
-        val = False
+
+        self.val = False
+
         try:
             self.acc_edit.edit_user(username,password,name,role,email,phone_number,address)
         except ValueError:
-            return render(request,"courseList.html",{'user': self.acc_edit,'check':val})
-        val = True
+            return render(request,"courseList.html",{'user': self.acc_edit,'check':self.val})
+        self.val = True
         updated_acc = User.objects.get(pk=self.acc_pk)
-        return render(request,"courseList.html",{'user': updated_acc })
+        return render(request,"courseList.html",{'user': updated_acc, 'check':self.val })
 
             
