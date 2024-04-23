092987a5f477d3f3c4f1b11495383cbe23d5d9f1
wilderb <blakew@uwm.edu>
2024-04-22 21:27:07 -0500

    fixing up issue with models/migrations

--- ta_app/views.py
+++ ta_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from ta_app.models import User, Section, Course
+from classes import UserClass
 
 # Create your views here.
+class AccountCreation(View):
+    def get(self, request):
+        return render(request, "createAccount.html", {})
+
+    def post(self, request):
+        pass
