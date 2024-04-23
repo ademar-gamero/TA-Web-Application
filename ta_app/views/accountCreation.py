c3032e1ddba14b7f877d821054ee839325684078
wilderb <blakew@uwm.edu>
2024-04-23 00:54:24 -0500

    testing accountCreation

--- ta_app/views/accountCreation.py
+++ ta_app/views/accountCreation.py
@@ -9,7 +9,17 @@ from ta_app.Classes.UserClass import UserClass
 class accountCreation(View):
 
     def get(self, request):
-        pass
+        current = request.session["role"]
+        if current == "Admin":
+            return render(request, "createAccount.html")
+        else:
+            raise PermissionDenied
 
     def post(self, request):
-        pass
+        try:
+            user = UserClass(request.POST['username'], request.POST['password'],
+                             request.POST['name'], request.POST['role'], request.POST['email'],
+                             request.POST['phone_number'], request.POST['address'])
+            user.create_user()
+        except ValueError as error:
+            return render(request, 'createAccount.html', {'message': error})
