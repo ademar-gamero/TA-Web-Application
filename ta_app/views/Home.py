28102870c550e5ae98edc2d7e1f9ec59e0f4d8b9
mnewton71 <matthewnewtonx71@gmail.com>
2024-04-23 22:38:59 -0500

    minor typo fixes

--- ta_app/views/Home.py
+++ ta_app/views/Home.py
@@ -1,9 +1,18 @@
 
 from django.shortcuts import render,redirect
 from django.views import View
+from django.contrib import messages
+from django.urls import reverse
 
 class Home(View):
     def get(self,request):
         curr_role = request.session["role"]
         curr_name = request.session["name"]
         return render(request, "home.html",{"role":curr_role,"name":curr_name})
+
+    def post(self,request):
+        if 'logout' in request.POST:
+            request.session.clear()
+            messages.success(request, "You have been logged out successfully.")
+            return redirect('login')
+        return self.get(request)
