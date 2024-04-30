e2ad10a86874872bc974dbe2b978da5b530e3ebb
mnewton71 <matthewnewtonx71@gmail.com>
2024-04-29 19:45:30 -0500

    fixed unauthorized access on home page

--- ta_app/views/Home.py
+++ ta_app/views/Home.py
@@ -3,15 +3,25 @@ from django.views import View
 from django.contrib import messages
 from django.urls import reverse
 
+
 class Home(View):
-    def get(self,request):
+    def get(self, request):
+        if 'role' not in request.session or 'name' not in request.session:
+            messages.error(request, "You are not logged in.")
+            return redirect('login')
+
         curr_role = request.session["role"]
         curr_name = request.session["name"]
-        return render(request, "home.html",{"role":curr_role,"name":curr_name})
+        return render(request, "home.html", {"role": curr_role, "name": curr_name})
 
-    def post(self,request):
+        response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        response['Pragma'] = 'no-cache'
+        response['Expires'] = '0'
+        return response
+
+    def post(self, request):
         if 'logout' in request.POST:
-            request.session.clear()
+            request.session.flush()
             messages.success(request, "You have been logged out successfully.")
             return redirect('login')
         return self.get(request)
