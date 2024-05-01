0fadd7906380527a7cab017f2df2819d296eaefc
LNelson100 <nelso862@uwm.edu>
2024-04-30 20:07:59 -0500

    started adjusting createCourse acceptance tests. Not complete.

--- ta_app/acceptanceTest/createCourseAcceptanceTests.py
+++ ta_app/acceptanceTest/createCourseAcceptanceTests.py
@@ -1,5 +1,4 @@
 from django.test import TestCase, Client
-
 from ta_app.models import User, Course
 
 
@@ -21,17 +20,33 @@ class CreateCourseAcceptanceTests(TestCase):
         resp = self.client.get("/Home/createCourse/")
         self.assertEqual(200, resp.status_code, "role error")  # check that the courseCreate page loads
 
+    def test_createCourseTAAccess(self):
+        # Test that the create course page will not load successfully if not an admin
+        self.ta = User.objects.create(name='Jane Smith', username='jsmith', password='password123',
+                                         email='jsmith@uwm.edu', role='Teacher-Assistant', phone_number='1234567890',
+                                         address='Home', assigned=False)
+        self.ta.save()
+        resp = self.client.post("/login/", {"username": self.ta.username, "password": self.ta.password},
+                                follow=True)
+        self.assertRedirects(resp, "/Home/", 302, msg_prefix="unsuccessful login")
+        resp = self.client.get("/Home/createCourse/") # try to go to the createCourse page. Should not be allowed.
+        self.assertRedirects(resp, "/Home/", 302,
+                             msg_prefix="incorrectly allowed to access createCourse")
+
     def test_createCourseSuccess(self):
         # Test creating a single course successfully
         data = {
             'course_id': 123,
             'course_name': 'test course',
-            'description': 'This is a test course.'
+            'description': 'This is a test course.',
+            'semester': 'Fall'
         }
         self.client.post("/login/", {"username": self.admin.username, "password": self.admin.password},
                          follow=True)  # try login
         resp = self.client.post('/Home/createCourse/', data)
-        self.assertTrue(resp.context['check'], "Course did not create correctly")
+        self.assertEqual(resp.context['errorMessage'], "Course Created Successfully!",
+                          "Course did not create correctly")
+        self.assertEqual(1, Course.objects.all().count(), "New course was not created")
 
 
 class CreateMultipleCourses(TestCase):
@@ -42,17 +57,19 @@ class CreateMultipleCourses(TestCase):
                                          address='Home', assigned=False)
         self.admin.save()
         # put a single course into the database
-        Course.objects.create(course_id=123, course_name='test course', description='This is a test').save()
+        Course.objects.create(course_id=123, course_name='test course', description='This is a test', semester='Fall').save()
 
     def test_createMultipleDifferent(self):
         # will adding multiple courses with all different fields create separate entries in the database?
         data = {
             'course_id': 361,
             'course_name': 'CompSci',
-            'description': 'This is a computer science course.'
+            'description': 'This is a computer science course.',
+            'semester': 'Fall'
         }
         resp = self.client.post('/Home/createCourse/', data)  # try adding again
-        self.assertTrue(resp.context['check'], "Course did not create correctly")
+        self.assertEqual(resp.context['errorMessage'], "Course Created Successfully!",
+                         "Course did not create correctly")
         # check the count of all courses in the database. (It should increase. Refer to test_createCourseSuccess
         # for the results single create course.)
         self.assertEqual(2, Course.objects.all().count(), "New course was not created")
@@ -63,10 +80,12 @@ class CreateMultipleCourses(TestCase):
         data = {
             'course_id': 123,
             'course_name': 'test course',
-            'description': 'This is a test course.'
+            'description': 'This is a test course.',
+            'semester': 'Fall'
         }
         resp = self.client.post('/Home/createCourse/', data)  # try adding again
-        self.assertFalse(resp.context['check'], "Duplicate course was incorrectly added")
+        self.assertEqual(resp.context['errorMessage'], "Duplicate course not added!",
+                         "Duplicate course was incorrectly added")
         # check the count of all courses in the database
         self.assertEqual(1, Course.objects.all().count(), "Duplicate course was added")  # should not increase
 
@@ -75,32 +94,53 @@ class CreateMultipleCourses(TestCase):
         data = {
             'course_id': 100,
             'course_name': 'test course',
-            'description': 'A different test course.'
+            'description': 'A different test course.',
+            'semester': 'Fall'
         }
         resp = self.client.post('/Home/createCourse/', data)
-        self.assertTrue(resp.context['check'], "Course did not create correctly")
+        self.assertEqual(resp.context['errorMessage'], "Course Created Successfully!",
+                         "Course did not create correctly")
         self.assertEqual(2, Course.objects.all().count(), "New course was not created")
 
     def test_createTwoWithOnlySameId(self):
-        # will trying to add another course with the same course_id but different course_name and description be allowed?
+        # will trying to add another course with the same course_id but different course_name, description,
+        # and semester be allowed?
         data = {
             'course_id': 123,
             'course_name': 'new name',
-            'description': 'A different test course.'
+            'description': 'A different test course.',
+            'semester': 'Spring'
         }
         resp = self.client.post('/Home/createCourse/', data)
-        self.assertTrue(resp.context['check'], "Course did not create correctly")
+        self.assertEqual(resp.context['errorMessage'], "Course Created Successfully!",
+                         "Course did not create correctly")
         self.assertEqual(2, Course.objects.all().count(), "New course was not created")  # should increase
 
     def test_createTwoWithOnlyDifferentDescription(self):
-        # will trying to add another course with the same course_id but different course_name and description be allowed?
+        # will trying to add another course with the same course_id, course_name, and semester, but different
+        # description be allowed?
         data = {
             'course_id': 123,
             'course_name': 'test course',
-            'description': 'A different test course.'
+            'description': 'A different test course.',
+            'semester': 'Fall'
         }
         resp = self.client.post('/Home/createCourse/', data)
-        self.assertFalse(resp.context['check'], "Course did not create correctly")
+        self.assertEqual(resp.context['errorMessage'], "Duplicate course not added!",
+                         "Duplicate course was incorrectly added")
         self.assertEqual(1, Course.objects.all().count(), "New course was not created")  # should not increase
 
+    def test_createTwoWithOnlyDifferentSemester(self):
+        # will trying to add another course with the same course_id, course_name, and description, but
+        # different semester be allowed?
+        data = {
+            'course_id': 123,
+            'course_name': 'test course',
+            'description': 'This is a test course.',
+            'semester': 'Spring'
+        }
+        resp = self.client.post('/Home/createCourse/', data)
+        self.assertEqual(resp.context['errorMessage'], "Course Created Successfully!",
+                         "Course did not create correctly")
+        self.assertEqual(2, Course.objects.all().count(), "New course was not created")  # should increase
 
