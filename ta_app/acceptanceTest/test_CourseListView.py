dddbf54a6fdc48e402f34ead1ea9bb409339b893
ademar <aegamero@uwm.edu>
2024-05-03 21:08:05 -0500

    fix migrations, add more tests for courselist because of semester field addition

--- ta_app/acceptanceTest/test_CourseListView.py
+++ ta_app/acceptanceTest/test_CourseListView.py
@@ -9,9 +9,9 @@ class courseList(TestCase):
     courseList=None
     def setUp(self):
         self.green = Client()
-        self.courselist = {351: ["compsci", "this course covers algos and data structs"],
-                      361: ["compsci", "this course covers software engineering"],
-                      102: ["psych", "this course covers psychology"]}
+        self.courselist = {351: ["compsci", "this course covers algos and data structs","Summer"],
+                      361: ["compsci", "this course covers software engineering","Winter"],
+                      102: ["psych", "this course covers psychology","Summer"]}
         User(name="admin",username="admin",password="admin",email="admin@email.com",role="Admin",phone_number=1,address="1",assigned=False).save()
         User(name="instructor",username="instructor",password="instructor",email="instructor@email.com",role="Instructor",phone_number=1,address="1",assigned=False).save()
         self.Ausername = "admin"
@@ -20,6 +20,7 @@ class courseList(TestCase):
         self.Ipassword = "instructor"
         for i in self.courselist.keys():
             y = 0
+            semester = "nothing"
             name = "nothing"
             descrip = "nothing"
             for x in self.courselist[i]:
@@ -27,8 +28,10 @@ class courseList(TestCase):
                     name = x
                 if(y == 1):
                     descrip = x
+                if(y == 2):
+                    semester = x
                 y = y + 1
-            Course(course_id=i,course_name=name,description=descrip).save()
+            Course(course_id=i,course_name=name,description=descrip,semester=semester).save()
 
     def test_accessAdmin(self):
         resp = self.green.post("/login/",{"username":self.Ausername,"password":self.Apassword},follow=True)
@@ -98,4 +101,38 @@ class courseList(TestCase):
         resp = self.green.post("/Home/courseList/",{"course_id":search_courseid,"course_name":search_course},follow=True)
         for j in resp.context["courselist"]:
             self.assertEqual(j.course_name,search_course,"search did not work")
+
+    def test_searchtriple(self):
+        search_courseid = 351
+        search_course = "compsci"
+        search_semester = "Summer"
+        resp = self.green.post("/Home/courseList/",{"course_id":search_courseid,"course_name":search_course,"semester":search_semester},follow=True)
+        for j in resp.context["courselist"]:
+            self.assertEqual(j.course_name,search_course,"search did not work")
+        clist = resp.context['courselist']
+        self.assertTrue(len(clist) == 1)
+
+    def test_searchNothing(self):
+        search_courseid = ""
+        search_course = ""
+        search_semester = ""
+        resp = self.green.post("/Home/courseList/",{"course_id":search_courseid,"course_name":search_course,"semester":search_semester},follow=True)
+        clist = resp.context['courselist']
+        self.assertTrue(len(clist) == 3)
+
+    def test_searchSemester(self):
+        search_semester = "Summer"
+        resp = self.green.post("/Home/courseList/",{"semester":search_semester},follow=True)
+        for j in resp.context["courselist"]:
+            self.assertEqual(j.semester,search_semester,"search did not work")
+        clist = resp.context['courselist']
+        self.assertTrue(len(clist) == 2)
+
+    def test_searchSemesterInvalid(self):
+        search_semester = "Spring"
+        resp = self.green.post("/Home/courseList/",{"semester":search_semester},follow=True)
+        clist = resp.context['courselist']
+        self.assertTrue(len(clist) == 0)
+
+
 
