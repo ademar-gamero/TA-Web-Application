347eb510b011c5ff18bf04ab5aafd71e6e9a3345
mnewton71 <matthewnewtonx71@gmail.com>
2024-05-06 19:43:10 -0500

    added tests for deleteCourse.py, wrote tests for deleteAccount.py

---
+++ ta_app/acceptanceTest/test_DeleteAccount.py
@@ -0,0 +1,52 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from ta_app.models import User
+from django.contrib.messages import get_messages
+
+class DeleteAccountTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin = User.objects.create(username='admin', password='adminpass', role='Admin')
+        self.user = User.objects.create(username='user1', password='userpass', role='TA')
+        self.delete_url = reverse('deleteAccount', kwargs={'pk': self.user.id})
+        self.login_url = reverse('login')
+
+    def simulate_login(self, username, password):
+        return self.client.post(self.login_url, {'username': username, 'password': password})
+
+    def test_admin_can_access_delete_page(self):
+        self.simulate_login('admin', 'adminpass')
+        response = self.client.get(self.delete_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'delete_account.html')
+
+    def test_confirm_deletion_by_admin(self):
+        self.simulate_login('admin', 'adminpass')
+        response = self.client.post(self.delete_url, {'confirm': 'yes'})
+        self.assertRedirects(response, reverse('accountList'))
+        self.assertFalse(User.objects.filter(id=self.user.id).exists())
+
+    def test_access_denied_for_non_admin(self):
+        self.simulate_login('user1', 'userpass')
+        response = self.client.get(self.delete_url)
+        self.assertNotEqual(response.status_code, 200)  # Expected redirection or access denied
+        self.assertTrue(any(msg.message == 'You are not authorized to view this page.' for msg in get_messages(response.wsgi_request)))
+
+    def test_attempt_to_delete_nonexistent_account(self):
+        self.simulate_login('admin', 'adminpass')
+        nonexistent_user_url = reverse('deleteAccount', kwargs={'pk': 999})  # ID that does not exist
+        response = self.client.post(nonexistent_user_url, {'confirm': 'yes'})
+        self.assertEqual(response.status_code, 404, "Expected a 404 status for a nonexistent account.")
+
+    def test_delete_without_confirmation(self):
+        self.simulate_login('admin', 'adminpass')
+        response = self.client.post(self.delete_url)  # Posting without the 'confirm' key
+        self.assertTrue(User.objects.filter(id=self.user.id).exists(),
+                        "User should still exist if deletion is not confirmed.")
+        self.assertTemplateUsed(response, 'delete_account.html',
+                                "Confirm page should still be shown when deletion is not confirmed.")
+        messages = list(get_messages(response.wsgi_request))
+        self.assertTrue('Account deleted successfully!' not in [msg.message for msg in messages],
+                        "No deletion success message should be present.")
+
+
--- ta_app/acceptanceTest/test_DeleteCourseView.py
+++ ta_app/acceptanceTest/test_DeleteCourseView.py
@@ -1,22 +1,51 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from ta_app.models import Course, User
+from django.contrib.messages import get_messages
 
 class DeleteCourseAcceptanceTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.admin = User.objects.create(username='admin', password='adminpass', role='Admin')
-        self.admin.save()
-        self.course = Course.objects.create(course_id=361, course_name="CS")
+        self.course = Course.objects.create(course_id=100, course_name="Sample Course")
         self.course.save()
+        self.delete_url = reverse('deleteCourse', kwargs={'course_id': self.course.pk})
+        self.login_url = reverse('login')
+
+    def simulate_login(self, username, password):
+        return self.client.post(self.login_url, {'username': username, 'password': password})
 
     def test_admin_can_access_delete_page(self):
-        self.client.login(username='admin', password='adminpass')
-        response = self.client.get(reverse('deleteCourse', args=[self.course.pk]))
+        self.simulate_login('admin', 'adminpass')
+        response = self.client.get(self.delete_url)
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'delete_course.html')
 
-    def test_confirm_deletion(self):
-        self.client.login(username='admin', password='adminpass')
-        response = self.client.post(reverse('deleteCourse', args=[self.course.pk]), {'confirm': '1'})
+    def test_confirm_deletion_by_admin(self):
+        self.simulate_login('admin', 'adminpass')
+        response = self.client.post(self.delete_url, {'confirm': '1'})
         self.assertRedirects(response, reverse('courseList'))
         self.assertEqual(Course.objects.count(), 0)
+
+    def test_access_denied_for_non_admin(self):
+        non_admin = User.objects.create(username='user', password='userpass', role='TA')
+        self.simulate_login('user', 'userpass')
+        response = self.client.get(self.delete_url)
+        self.assertRedirects(response, reverse('courseList'), status_code=302, target_status_code=200)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertIn('You are not authorized to view this page.', [msg.message for msg in messages])
+
+    def test_attempt_to_delete_nonexistent_account(self):
+        self.simulate_login('admin', 'adminpass')
+        nonexistent_user_url = reverse('deleteAccount', kwargs={'pk': 999})  # ID that does not exist
+        response = self.client.post(nonexistent_user_url, {'confirm': 'yes'})
+        self.assertRedirects(response, reverse('accountList'))
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), 'User account not found!')
+
+    def test_delete_without_confirmation(self):
+        self.simulate_login('admin', 'adminpass')
+        response = self.client.post(self.delete_url)
+        self.assertEqual(Course.objects.count(), 1)
+        self.assertRedirects(response, reverse('courseList'))
