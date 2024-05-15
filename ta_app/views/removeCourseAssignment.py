d8094c96fcfcc2a86cea378401a5567d69dbb510
wblak <blakew@uwm.edu>
2024-05-14 23:10:27 -0500

    updating urls for RemoveCourseAssignment, making stubs for view

---
+++ ta_app/views/removeCourseAssignment.py
@@ -0,0 +1,13 @@
+from django.views import View
+from django.shortcuts import render, redirect
+from ta_app.models import Course
+from django.contrib import messages
+
+
+class removeCourseAssignment(View):
+
+    def get(self, request, user_pk, section_pk):
+        pass
+
+    def post(self, request, user_pk, section_pk):
+        pass
